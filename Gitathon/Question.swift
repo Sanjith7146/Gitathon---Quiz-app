a62990db40b66d498950385425c6137b21571460
Sanjith7146 <sanjith7146@gmail.com>
2021-06-19 11:38:02 +0800

    Milestone 1: Variable Answers

---
+++ Gitathon/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Gitathon
+//
+//  Created by sanjithsethu on 19/6/21.
+//
+
+import Foundation
+
+struct Question {
+    var title: String
+    
+    var option1: String
+    var option2: String
+    var option3: String
+    var option4: String
+    
+    var correctOption: Int
+}
+
