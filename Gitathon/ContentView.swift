72a4c8aa8a763b14ed3157298ac12d6206b944d0
Sanjith7146 <sanjith7146@gmail.com>
2021-06-19 11:30:02 +0800

    Initial Commit

---
+++ Gitathon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gitathon
+//
+//  Created by sanjithsethu on 19/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gitathon/GitathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitathonApp.swift
+//  Gitathon
+//
+//  Created by sanjithsethu on 19/6/21.
+//
+
+import SwiftUI
+
+@main
+struct GitathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
